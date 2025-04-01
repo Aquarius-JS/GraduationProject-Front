9aa10c4725c3675268cc9a25d90587a2d08ad599
aquarius <1015772292@qq.com>
2025-04-01 23:58:30 +0800

    feat(公告预览页附件)

--- src/announcement/View.jsx
+++ src/announcement/View.jsx
@@ -10,6 +10,20 @@ export default function () {
       <Style name="announcement" />
       <div className="announcement-container">
         <div className="announcement-info" dangerouslySetInnerHTML={{ __html: state.announcementInfo?.content }} />
+        {state.announcementInfo?.attached_file_list_info && (
+          <div className="attached-file">
+            <p style={{ color: 'gray' }}>附件:</p>
+            {state.announcementInfo.attached_file_list_info.map(item => {
+              return (
+                <div style={{ margin: '5px 10px' }}>
+                  <a href={item.url} target="_blank">
+                    {item.name}
+                  </a>
+                </div>
+              );
+            })}
+          </div>
+        )}
       </div>
     </>
   );
