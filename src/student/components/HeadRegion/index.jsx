ded38edaf70cfd7efc61993d269508090e7a9ed5
aquarius <1015772292@qq.com>
2025-04-22 23:06:33 +0800

    feat(搜索框)

--- src/student/components/HeadRegion/index.jsx
+++ src/student/components/HeadRegion/index.jsx
@@ -1,16 +1,32 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useModelState } from 'react-imvc/hook';
-import { Avatar, Badge, Drawer } from 'antd';
+import { Avatar, Badge, Drawer, Input, Spin, Empty } from 'antd';
 import { UserOutlined, BellOutlined, LinkOutlined } from '@ant-design/icons';
 import { formatUnixToDate } from '../../../share/formatUnix';
+import { debounce } from 'lodash';
 
 export default function () {
   const state = useModelState();
   const userInfo = state.userInfo;
   const violationInfoList = state.violationInfoList;
   const announcementBasicInfo = state.announcementBasicInfo;
-
+  const [computedAnnBasicInfo, setComputedAnnBasicInfo] = useState([]); //公告通知列表
   const [annDrawerSwitch, setAnnDrawerSwitch] = useState(false); //通知框开关
+  const [annIsSearching, setAnnIsSearching] = useState(false);
+
+  useEffect(() => {
+    setComputedAnnBasicInfo(announcementBasicInfo);
+  }, [announcementBasicInfo]);
+
+  const computeAnnBasicInfo = debounce(searchValue => { //TODO: fix防抖bug
+    if (searchValue === '') {
+      setComputedAnnBasicInfo(announcementBasicInfo);
+    } else {
+      setComputedAnnBasicInfo(announcementBasicInfo?.filter(item => item.title.includes(searchValue)) ?? []);
+    }
+    setAnnIsSearching(false);
+  }, 1300);
+
   return (
     <>
       <div className="header-container">
@@ -44,22 +60,39 @@ export default function () {
         }}
         open={annDrawerSwitch}
       >
-        {announcementBasicInfo?.map(item => {
-          return (
-            <div
-              className="announcement-item"
-              key={item.id}
-              onClick={() => {
-                window.open(`/announcement/exhibition?announcementId=${item.id}`, '_blank');
-              }}
-            >
-              <div className="announcement-title-container">
-                <LinkOutlined /> <span className="announcement-title">{item.title}</span>
-              </div>
-              <span className="announcement-date">{`[${formatUnixToDate(item.publish_time)}]`}</span>
-            </div>
-          );
-        })}
+        <Input
+          size="small"
+          placeholder="输入标题进行搜索"
+          allowClear={true}
+          className="announcement-search"
+          onChange={e => {
+            setAnnIsSearching(true);
+            computeAnnBasicInfo(e.target.value);
+          }}
+        />
+        <Spin spinning={annIsSearching}>
+          <div className="announcement-list">
+            {computedAnnBasicInfo?.map(item => {
+              return (
+                <div
+                  className="announcement-item"
+                  key={item.id}
+                  onClick={() => {
+                    window.open(`/announcement/exhibition?announcementId=${item.id}`, '_blank');
+                  }}
+                >
+                  <div className="announcement-title-container">
+                    <LinkOutlined /> <span className="announcement-title">{item.title}</span>
+                  </div>
+                  <span className="announcement-date">{`[${formatUnixToDate(item.publish_time)}]`}</span>
+                </div>
+              );
+            })}
+            {computedAnnBasicInfo?.length === 0 && (
+              <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} description="公告信息为空" />
+            )}
+          </div>
+        </Spin>
       </Drawer>
     </>
   );
